27cbc7b206d834423f06e44b624bdf61f4d29df2
andi balo <andibalo213@gmail.com>
2021-10-18 00:26:11 +0700

    created auth modal and modal tabs

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -47,7 +47,10 @@
           <div class="flex justify-between items-center pb-4">
             <p class="text-2xl font-bold">Your Account</p>
             <!-- Modal Close Button -->
-            <div class="modal-close cursor-pointer z-50">
+            <div
+              class="modal-close cursor-pointer z-50"
+              @click.prevent="toggleAuthModal"
+            >
               <i class="fas fa-times"></i>
             </div>
           </div>
@@ -56,31 +59,32 @@
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="
-                  block
-                  rounded
-                  py-3
-                  px-4
-                  transition
-                  hover:text-white
-                  text-white
-                  bg-blue-600
-                "
+                class="block rounded py-3 px-4 transition"
                 href="#"
+                @click.prevent="tab = 'login'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register',
+                }"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-blue-600"
+                class="block rounded py-3 px-4 transition"
                 href="#"
+                @click.prevent="tab = 'register'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login',
+                }"
                 >Register</a
               >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -139,7 +143,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -293,12 +297,17 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   name: "AuthModal",
+  data() {
+    return {
+      tab: "login",
+    };
+  },
   computed: {
-    ...mapGetters(["authModalShow"]),
+    ...mapState(["authModalShow"]),
   },
   methods: {
     ...mapMutations(["toggleAuthModal"]),
