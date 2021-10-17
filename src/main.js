9ff9cf1d3562b7aec84074e2455e5d3199a994d3
andi balo <andibalo213@gmail.com>
2021-10-17 23:39:29 +0700

    initialized cvue project, integrated html template

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/* eslint-disable quotes */
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/tailwind.css";
+import "./assets/main.css";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable quotes */
+module.exports = {
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem"
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
