776f0e5557aed1c476516baf7d6479cd2f9b9c88
andi balo <andibalo213@gmail.com>
2021-10-22 08:35:22 +0700

    added and validated login/registration form

---
+++ src/includes/validation.js
@@ -0,0 +1,55 @@
+import {
+  Field as VeeField,
+  Form as VeeForm,
+  defineRule,
+  ErrorMessage,
+  configure
+} from "vee-validate";
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  min_value as minimumValue,
+  max_value as maximumValue,
+  confirmed
+} from "@vee-validate/rules";
+
+export default {
+  install(app) {
+    app.component("VeeField", VeeField);
+    app.component("VeeForm", VeeForm);
+    app.component("ErrorMessage", ErrorMessage);
+
+    defineRule("required", required);
+    defineRule("tos", required);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alpha_spaces", alphaSpaces);
+    defineRule("email", email);
+    defineRule("min_value", minimumValue);
+    defineRule("max_value", maximumValue);
+    defineRule("passwords_mismatch", confirmed);
+
+    configure({
+      generateMessage: ctx => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alpha_spaces: `The field ${ctx.field} must only contain alphabets and spaces.`,
+          email: `The field ${ctx.field} must be in valid email format.`,
+          passwords_mismatch: "Passwords do not match",
+          tos: `You must accept the terms of service`
+        };
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid`;
+
+        return message;
+      }
+    });
+  }
+};
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VeevalidatePlugin from "./includes/validation";
 import "./assets/tailwind.css";
 import "./assets/main.css";
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(VeevalidatePlugin)
   .mount("#app");
