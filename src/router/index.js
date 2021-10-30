df56680e5f9a5e4d64adc5d36adefb1c78232f72
andi balo <andibalo213@gmail.com>
2021-10-30 13:14:20 +0700

    created route for manage page, added global navigation guard for protected pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import store from "@/store";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: "/",
+    name: "Home",
+    component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
+  {
+    path: "/manage",
+    name: "Manage",
+    meta: {
+      requiresAuth: true
+    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Manage.vue")
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: {
+      name: "Home"
+    }
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500"
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.some((route) => route.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: "Home" });
+  }
 });
 
 export default router;
